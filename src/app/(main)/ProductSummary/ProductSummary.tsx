7aa566341d3c0d67022001eb74f6f0649d5fd6c2
atheimuz <atheimuz@naver.com>
2025-01-06 22:19:22 +0900

    feat: 선택된 상품의 데이터 표시

--- src/app/(main)/ProductSummary/ProductSummary.tsx
+++ src/app/(main)/ProductSummary/ProductSummary.tsx
@@ -10,16 +10,11 @@ const ProductSummary = () => {
 
     return (
         <div className={styles.wrapper}>
-            <a href="https://ohou.se/productions/1803860/selling" className={styles.link}>
-                <img
-                    className={styles.thumbnail}
-                    src="https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/162849292369220740.jpg?w=100"
-                />
+            <a href={summary.link} className={styles.link} target="_blank">
+                <img className={styles.thumbnail} src={summary.thumbnail} />
                 <div className={styles.textBox}>
-                    <p className={styles.title}>
-                        미나 MINI LED 버섯램프 무드등 조명 LH60 (C타입충전)
-                    </p>
-                    <p className={styles.desc}>-10% 45,000원. 리뷰 4.8점, 4,543개</p>
+                    <p className={styles.title}>{summary.title}</p>
+                    <p className={styles.desc}>{summary.description}</p>
                 </div>
             </a>
         </div>
