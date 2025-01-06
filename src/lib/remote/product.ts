5379b13dc1c26f6e330990a03f708f5349f0f4e2
atheimuz <atheimuz@naver.com>
2025-01-06 22:13:48 +0900

    feat: 타입 명시

--- src/lib/remote/product.ts
+++ src/lib/remote/product.ts
@@ -1,8 +1,10 @@
+import { IProduct } from "@/models/product";
+
 export const getProductAPI = async (link: string) => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/api/product?link=${link}`);
         const resJson = await res.json();
-        const data = resJson.data;
+        const data: IProduct = resJson.data;
 
         return data;
     } catch (e) {
