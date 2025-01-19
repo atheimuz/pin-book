c31f751bf5cf245af5c141d4ee73559cff22f2fd
atheimuz <atheimuz@naver.com>
2025-01-19 23:02:27 +0900

    feat: book 404 응답 추가

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -9,7 +9,15 @@ export async function GET(request: Request, { params }: { params: { id: string }
         const { id } = params;
         const bookId = new mongoose.Types.ObjectId(id);
         const book = await Book.findOne({ _id: bookId }).select(["_id", "image", "pins"]);
-        return NextResponse.json({ status: 200, data: book });
+
+        if (!book) {
+            return NextResponse.json(
+                { success: false, message: "Book not found." },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ success: true, data: book }, { status: 200 });
     } catch (error) {
         const err = error as Error;
         return NextResponse.json(
