8cc168f1160688d39b26120754a2c7d2e050ebe1
atheimuz <atheimuz@naver.com>
2025-01-22 18:53:01 +0900

    feat: book 생성 api 추가

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -29,3 +29,34 @@ export async function GET(request: Request, { params }: { params: { id: string }
         );
     }
 }
+
+export async function POST(request: Request) {
+    await connectToDatabase();
+
+    try {
+        const body = await request.json();
+        const image = "https://prs.ohouse.com/apne2/any/v1-313723627950208.jpg?w=720";
+
+        // 이미지 업로드 과정 필요
+        // ...
+
+        const newBook = new Book({
+            image: image,
+            pins: body.pins || [],
+            createdAt: new Date()
+        });
+
+        await newBook.save();
+
+        return NextResponse.json({ success: true, data: newBook }, { status: 201 });
+    } catch (error) {
+        const err = error as Error;
+        return NextResponse.json(
+            {
+                success: false,
+                message: `Failed to create book. detail: ${err.message}`
+            },
+            { status: 500 }
+        );
+    }
+}
