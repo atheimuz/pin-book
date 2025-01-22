8a7caf9d667d61818ab371d80636dc3e4faaf7dd
atheimuz <atheimuz@naver.com>
2025-01-22 19:06:27 +0900

    feat: book 정보 가져오는 api 분리

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,34 +1,6 @@
 import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/mongodb";
-import mongoose from "mongoose";
 import Book from "@/models/book";
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    await connectToDatabase();
-    try {
-        const { id } = params;
-        const bookId = new mongoose.Types.ObjectId(id);
-        const book = await Book.findOne({ _id: bookId }).select(["_id", "image", "pins"]);
-
-        if (!book) {
-            return NextResponse.json(
-                { success: false, message: "Book not found." },
-                { status: 404 }
-            );
-        }
-
-        return NextResponse.json({ success: true, data: book }, { status: 200 });
-    } catch (error) {
-        const err = error as Error;
-        return NextResponse.json(
-            {
-                success: false,
-                message: `Error fetching book. detail: ${err.message}`
-            },
-            { status: 500 }
-        );
-    }
-}
 
 export async function POST(request: Request) {
     await connectToDatabase();
